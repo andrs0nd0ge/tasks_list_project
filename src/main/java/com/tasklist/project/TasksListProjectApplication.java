a576c49c5cdef01a41e661f3de1fbec50ad51fac
Maxim Farafonov <anderson.utuber@gmail.com>
2023-03-28 18:00:40 +0600

    Initialized project

---
+++ src/main/java/com/tasklist/project/TasksListProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tasklist.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksListProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TasksListProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tasklist/project/TasksListProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasklist.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksListProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
